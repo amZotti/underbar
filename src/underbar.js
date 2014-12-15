f3f9d4c6e70741496a27df5e5a3016b7bfa224a2
amzotti <azotti1337@gmail.com>
2014-12-14 23:17:31 -0800

    Implement identity() and last() functions
    Chose to use a conditional statement while implementing last() rather
    than a ternary operator to account for the extra check to see if n is
    greater than the size of the array.

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,14 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var end = array.length;
+    var start = end - n;
+    if (n == undefined)
+      return array[end - 1];
+    else if (n >= end)
+      return array.slice();
+    else
+      return array.slice(start, end);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
